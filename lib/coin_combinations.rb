b680b1bb236fdf9c7d432a752cd5dd11f96d5cbe
Matt Carlson and Rony Nasr <matt@>
2016-06-30 10:09:21 -0700

    get tests passing

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -16,6 +16,7 @@ class Fixnum
     cents = self
     coins = { 25 => 0, 10 => 0, 5 => 0, 1 => 0}
     coins_names = { 25 => "quarter ", 10 => "dime ", 5 => "nickel ", 1 => "penny"}
+    plural_coins_names = { 25 => "quarters ", 10 => "dimes ", 5 => "nickels ", 1 => "pennies"}
     keys = coins.keys()
 
     keys.each do |key|
@@ -26,7 +27,11 @@ class Fixnum
     result_text = ""
     keys.each do |key|
       if coins[key] != 0
-        result_text += coins[key].to_s() + " " + coins_names[key]
+        if coins[key] > 1
+          result_text += coins[key].to_s() + " " + plural_coins_names[key]
+        else
+          result_text += coins[key].to_s() + " " + coins_names[key]
+        end
       end
     end
     result_text
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -17,4 +17,8 @@ describe('Fixnum#return_coins') do
   it("returns the number one for one cent") do
     expect((41).return_coins()).to(eq("1 quarter 1 dime 1 nickel 1 penny"))
   end
+
+  it('handles plural numbers') do
+    expect((78).return_coins()).to(eq("3 quarters 3 pennies"))
+  end
 end
